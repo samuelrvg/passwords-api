010a5c421d372b13f9738fadf15a959df34ff659
Samuel Vaz <samuel.rvg@gmail.com>
2019-09-25 01:03:53 -0300

    add swagger

--- Passwords.Api/Startup.cs
+++ Passwords.Api/Startup.cs
@@ -6,10 +6,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Passwords.Api.Data.Context;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Passwords.Api
 {
@@ -25,12 +28,29 @@ namespace Passwords.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Passwords", Version = "v1" });
+            });
+
+            services.AddDbContext<PasswordContext>(options => 
+                options//.UseLazyLoadingProxies()
+                .UseSqlServer(Configuration.GetConnectionString("passwordConnection")));
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Passwords v1");
+                c.RoutePrefix = string.Empty;
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
